3362bbecb4fbdaed2232cc986bda57f49a2c230d
Ismail Eid <ismacileid122@gmail.com>
2022-12-27 11:09:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const corsOptions = {
+  origin: 'http://localhost:8081'
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+  res.send({ message: "Welcome to bezkoder application!" })
+})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Process is running on port ${PORT}`)
+})
